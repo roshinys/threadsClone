be7934495e20aa1bfda6820bf7152613253aab5f
roshinys <roshinchaya@gmail.com>
2023-08-13 23:15:58 +0530

    fetched posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 //app/page.tsx
+import { fetchPosts } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
   return (
     <>
       {/* <UserButton afterSignOutUrl="/" /> */}
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10 ">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No Threads Found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => {
+              return <p className="text-light-2">new post</p>;
+            })}
+          </>
+        )}
+      </section>
     </>
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -31,7 +31,37 @@ export async function createThread({
       $push: { threads: createdThread._id },
     });
   } catch (err: any) {
-    console.log(err);
+    throw new Error(`Failed to create thread: ${err.message}`);
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+    // have no parents
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+    const posts = await postsQuery.exec();
+    const isNext = totalPostsCount > skipAmount + posts.length;
+    return { posts, isNext };
+  } catch (err: any) {
     throw new Error(`Failed to create thread: ${err.message}`);
   }
 }
